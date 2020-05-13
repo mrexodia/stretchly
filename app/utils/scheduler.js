e47e23457641c5c0adc7fcf264e517aa3d08fd6d
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-05-13 22:47:40 +0200

    Implement pause/resume functions in Scheduler that preserve the state

--- app/utils/scheduler.js
+++ app/utils/scheduler.js
@@ -5,14 +5,32 @@ class Scheduler {
     this.delay = delay
     this.func = func
     this.reference = reference
+    this.timeLeftWhenPaused = null
+    this.allowPause = false
   }
 
   get timeLeft () {
     if (this.timer === null) return false
+    if (this.timeLeftWhenPaused !== null) return this.timeLeftWhenPaused
     return this.now + this.delay - Date.now()
   }
 
-  plan () {
+  pause () {
+    if (!this.allowPause || this.timer === null || this.timeLeftWhenPaused !== null) return
+    clearTimeout(this.timer)
+    this.timeLeftWhenPaused = this.timeLeft
+    return true
+  }
+
+  resume () {
+    if (this.timer === null || this.timeLeftWhenPaused === null) return
+    this.delay = Math.max(100, this.timeLeftWhenPaused)
+    this.timeLeftWhenPaused = null
+    this.plan()
+  }
+
+  plan (allowPause = false) {
+    this.allowPause = allowPause
     this.now = Date.now()
     this.timer = setTimeout(this.func, this.delay)
   }
